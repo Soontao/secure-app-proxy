f0eca0ae2990777ef1d6f5acb580e72744f586bf
Theo Sun <theo.sun.github@outlook.com>
2023-04-09 20:39:28 +0800

    feat: update

--- main.go
+++ main.go
@@ -14,13 +14,14 @@ func main() {
 		log.Fatalf("parse upstream url failed %s", err)
 	}
 	log.Printf("upstream endpoint %s", u)
-	rp := httputil.NewSingleHostReverseProxy(u)
+	rp := &httputil.ReverseProxy{
+		Rewrite: func(pr *httputil.ProxyRequest) {
+			pr.SetURL(u)
+		},
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		rp.ServeHTTP(w, r)
 	})
-	rp.Rewrite = func(pr *httputil.ProxyRequest) {
-		pr.Out.Host = u.Host
-	}
 	log.Println("Listening on port: 8080")
 	http.ListenAndServe(":8080", nil)
 }
